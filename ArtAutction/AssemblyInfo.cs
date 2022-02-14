290dd7384ffe06c11dc8b02753d098574e5a7289
Oladayo Azeez <arhziz19@gmail.com>
2022-02-14 11:02:14 +0100

    updating the icon sources and using build action for embedded resources

--- ArtAutction/AssemblyInfo.cs
+++ ArtAutction/AssemblyInfo.cs
@@ -1,3 +1,6 @@
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("MaterialIconsOutlined-Regular.ttf", Alias = "MaterialOutlined")]
+[assembly: ExportFont("MaterialIcons-Regular.ttf", Alias = "Material")]
